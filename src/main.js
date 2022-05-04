21d669a9356b167f274d5b27b7a498a39b9c2b3a
Ernesto Mendoza <ernestogbh@gmail.com>
2022-05-04 16:18:18 -0500

    change

---
+++ src/main.js
@@ -0,0 +1,40 @@
+async function getTredingMoviesPreview(){
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+    const movies = data.results;
+    const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+
+    movies.forEach(movie => {
+
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path);
+
+        movieContainer.appendChild(movieImg);
+        trendingPreviewMoviesContainer.appendChild(movieContainer);
+    });
+};
+async function getCategoriesPreview(){
+    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+    const data = await res.json();
+    const categories = data.genres;
+    const previewCategoryContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
+
+    categories.forEach(category => {
+
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container')
+        const categoryTitle = document.createElement('h3')
+        categoryTitle.classList.add('category-title')
+        categoryTitle.setAttribute('id', 'id' + category.id)
+        categoryTitle.innerText = category.name;
+
+        categoryContainer.appendChild(categoryTitle);
+        previewCategoryContainer.appendChild(categoryContainer);
+    });
+};
+getTredingMoviesPreview();
+getCategoriesPreview();
